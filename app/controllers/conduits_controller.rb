11224f90d69bca652c39a8a1bc4c26bc7a3ec6ca
daisuke721 <d.magome.07@gmail.com>
2024-11-24 23:49:50 +0900

    feat: home,create,edit,articleのルーティングを設定 #1

--- app/controllers/conduits_controller.rb
+++ app/controllers/conduits_controller.rb
@@ -1,4 +1,13 @@
 class ConduitsController < ApplicationController
   def home
   end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def article
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'conduits/home'
+  get 'conduits/home', to: 'conduits#home'
+  get 'conduits/create', to: 'conduits#create'
+  get 'conduits/edit', to: 'conduits#edit'
+  get 'conduits/article', to: 'conduits#article'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
